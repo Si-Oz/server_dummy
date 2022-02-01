06603558e695ffa2a66b0830ec61b39e720fa105
SimonColman1 <simon@ozhairandbeauty.com>
2022-02-01 11:57:53 +1100

    config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
+
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-type',
+            value: 'application/liquid',
+          },
+        ],
+      },
+    ]
+  },
 }
+
