4a3a9dc5625cacbaabf46c2e4aeed5378dcd3ed3
SimonColman1 <simon@ozhairandbeauty.com>
2022-02-01 12:02:36 +1100

    changed back server

--- server.js
+++ server.js
@@ -1,44 +1,44 @@
-// // // server.js
-// // const { createServer } = require('http')
-// // const { parse } = require('url')
-// // const next = require('next')
+// server.js
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
 
-// // const dev = process.env.NODE_ENV !== 'production'
-// // const hostname = 'localhost'
-// // const port = 3000
-// // // when using middleware `hostname` and `port` must be provided below
-// // const app = next({ dev, hostname, port })
-// // const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = 'localhost'
+const port = 3000
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
 
 
-// // app.prepare().then(() => {
-// //   createServer((req, res) => {
-// //     // Be sure to pass `true` as the second argument to `url.parse`.
-// //     // This tells it to parse the query portion of the URL.
-// //     const parsedUrl = parse(req.url, true)
-// //     const { pathname, query } = parsedUrl
+app.prepare().then(() => {
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
 
-// //     res.setHeader('Content-type', 'application/liquid')
+    res.setHeader('Content-type', 'application/liquid')
 
-// //     if (pathname === '/a') {
-// //       app.render(req, res, '/a', query)
-// //     } else if (pathname === '/b') {
-// //       app.render(req, res, '/b', query)
-// //     } 
-// //     // else if(pathname === '/app'){
-// //     //     res.setHeader('Content-type', 'application/liquid')
-// //     //     console.log(res)
-// //     //     app.render(req,res, '/index', query)
+    if (pathname === '/a') {
+      app.render(req, res, '/a', query)
+    } else if (pathname === '/b') {
+      app.render(req, res, '/b', query)
+    } 
+    // else if(pathname === '/app'){
+    //     res.setHeader('Content-type', 'application/liquid')
+    //     console.log(res)
+    //     app.render(req,res, '/index', query)
         
-// //     // }
-// //     else {
-// //       handle(req, res, parsedUrl)
-// //     }
-// //   }).listen(process.env.PORT || port, (err) => {
-// //     if (err) throw err
-// //     console.log(`> Ready on http://${hostname}:${port}`)
-// //   })
-// // })
+    // }
+    else {
+      handle(req, res, parsedUrl)
+    }
+  }).listen(process.env.PORT || port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://${hostname}:${port}`)
+  })
+})
 
 // const express = require('express')
 // const next = require('next')
@@ -69,33 +69,33 @@
 // })
 
 
-const Koa = require('koa')
-const next = require('next')
-const Router = require('@koa/router')
+// const Koa = require('koa')
+// const next = require('next')
+// const Router = require('@koa/router')
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+// const port = parseInt(process.env.PORT, 10) || 3000
+// const dev = process.env.NODE_ENV !== 'production'
+// const app = next({ dev })
+// const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
-  const server = new Koa()
-  const router = new Router()
+// app.prepare().then(() => {
+//   const server = new Koa()
+//   const router = new Router()
 
-  router.all('(.*)', async (ctx) => {
-    await handle(ctx.req, ctx.res)
-    ctx.response.set('Content-Type', 'application/liquid');
-    console.log(ctx.response)
-    ctx.respond = false
-  })
+//   router.all('(.*)', async (ctx) => {
+//     await handle(ctx.req, ctx.res)
+//     ctx.response.set('Content-Type', 'application/liquid');
+//     console.log(ctx.response)
+//     ctx.respond = false
+//   })
 
-  server.use(async (ctx, next) => {
-    ctx.res.statusCode = 200
-    await next()
-  })
+//   server.use(async (ctx, next) => {
+//     ctx.res.statusCode = 200
+//     await next()
+//   })
 
-  server.use(router.routes())
-  server.listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+//   server.use(router.routes())
+//   server.listen(port, () => {
+//     console.log(`> Ready on http://localhost:${port}`)
+//   })
+// })
