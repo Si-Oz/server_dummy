43b7337bed6a0b3d33a1bf4b865afe7953e857f9
SimonColman1 <simon@ozhairandbeauty.com>
2022-02-01 13:10:15 +1100

    static build

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import 'https://simon-testing-environment.myshopify.com/a/1/_next/static/css/b3433cc32e91f401.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
